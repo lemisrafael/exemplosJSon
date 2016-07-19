a2e6aeb5738be8d966532c4addad3f47b5efcfa9
RafaelLemesS <rafael@salesoft.com.br>
2016-07-19 16:07:26 -0300

    enviando exemplo 4

---
+++ meusTutoriais.js
@@ -0,0 +1,14 @@
+minhaFuncao([
+	{
+		'display': 'Tutorial Javascript',
+		'url': 'http://www.w3schools.com/js/default.asp'
+	},
+	{
+		'display': 'Tutorial HTML',
+		'url': 'http://www.w3schools.com/html/default.asp'
+	},
+	{
+		'display': 'Tutorial CSS',
+		'url': 'http://www.w3schools.com/css/default.asp'
+	}
+])
